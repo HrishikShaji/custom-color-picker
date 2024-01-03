b23779f0f36eb1009b290dacc7adc01567262565
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-03 21:44:34 +0530

    slider added

--- app/components/ColorPalette.tsx
+++ app/components/ColorPalette.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import { Slider } from "./Slider";
+
 export const ColorPalette = () => {
-  return <div className="p-5 bg-neutral-700 rounded-md"></div>;
+	const [hue, setHue] = useState(0);
+	const [sat, setSat] = useState(0);
+	const [lum, setLum] = useState(0);
+	return (
+		<div className="p-5 bg-neutral-700 rounded-md">
+			<div
+				className="w-2 h-2"
+				style={{ backgroundColor: `hsl(${hue},${sat}%,${lum}%)` }}
+			></div>
+			<Slider hue={hue} sat={sat} lum={lum} setHue={setHue} setLum={setLum} setSat={setSat} />
+		</div>
+	);
 };
---
+++ app/components/Slider.tsx
@@ -0,0 +1,45 @@
+import { Dispatch, SetStateAction } from "react";
+
+interface SliderProps {
+  setHue: Dispatch<SetStateAction<number>>;
+  setSat: Dispatch<SetStateAction<number>>;
+  setLum: Dispatch<SetStateAction<number>>;
+  hue: number;
+  sat: number;
+  lum: number;
+}
+
+export const Slider: React.FC<SliderProps> = ({
+  setHue,
+  setSat,
+  setLum,
+  hue,
+  sat,
+  lum,
+}) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        value={hue}
+        type="range"
+        min="0"
+        max="360"
+        onChange={(e) => setHue(parseInt(e.target.value))}
+      />
+      <input
+        value={sat}
+        type="range"
+        min="0"
+        max="100"
+        onChange={(e) => setSat(parseInt(e.target.value))}
+      />
+      <input
+        value={lum}
+        type="range"
+        min="0"
+        max="100"
+        onChange={(e) => setLum(parseInt(e.target.value))}
+      />
+    </div>
+  );
+};
