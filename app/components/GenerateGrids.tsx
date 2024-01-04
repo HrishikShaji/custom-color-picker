5710beb4d1bfa34efdec63c6b4477151b6240f9c
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-04 15:00:20 +0530

    colorpicker stable

--- app/components/GenerateGrids.tsx
+++ app/components/GenerateGrids.tsx
@@ -1,43 +1,52 @@
+import { Dispatch, SetStateAction } from "react";
 import { Color } from "./ColorPalette";
 
-export const GenerateGrids = ({
-  hue,
-  sat,
-  lum,
-}: {
-  hue: number;
-  sat: number;
-  lum: number;
+interface GenerateGridsProps {
+	hue: number;
+	sat: number;
+	lum: number;
+	setSelectedColor: Dispatch<SetStateAction<string>>;
+	cols: number;
+}
+
+export const GenerateGrids: React.FC<GenerateGridsProps> = ({
+	hue,
+	sat,
+	lum,
+	cols,
+	setSelectedColor,
 }) => {
-  let grids: Color[][] = [];
-  for (let i = 0; i < 10; i++) {
-    grids[i] = [];
-    for (let j = 0; j < 10; j++) {
-      grids[i][j] = {
-        hue: hue,
-        sat: sat,
-        lum: lum,
-      };
-    }
-  }
-  return (
-    <div className="flex gap-1">
-      {grids.map((grid, i) => (
-        <div key={i} className="flex flex-col gap-1">
-          {grid.map((item, j) => {
-            console.log(item);
-            return (
-              <div
-                className="w-4 h-4 "
-                key={j}
-                style={{
-                  backgroundColor: `hsl(${item.hue},${item.sat}%,${item.lum}%)`,
-                }}
-              ></div>
-            );
-          })}
-        </div>
-      ))}
-    </div>
-  );
+	let grids: Color[][] = [];
+	for (let i = 0; i < cols; i++) {
+		grids[i] = [];
+		for (let j = 0; j < cols; j++) {
+			grids[i][j] = {
+				hue: hue,
+				sat: sat + j * ((100 - sat) / cols),
+				lum: lum + i * ((100 - lum) / cols),
+			};
+		}
+	}
+	return (
+		<div className="flex gap-1">
+			{grids.map((grid, i) => (
+				<div key={i} className="flex flex-col gap-1">
+					{grid.map((item, j) => {
+						return (
+							<div
+								onClick={() =>
+									setSelectedColor(`hsl(${item.hue},${item.sat}%,${item.lum}%)`)
+								}
+								className="w-4 h-4 "
+								key={j}
+								style={{
+									backgroundColor: `hsl(${item.hue},${item.sat}%,${item.lum}%)`,
+								}}
+							></div>
+						);
+					})}
+				</div>
+			))}
+		</div>
+	);
 };
--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -1,48 +1,56 @@
 import { Dispatch, SetStateAction } from "react";
 
 interface SliderProps {
-  setHue: Dispatch<SetStateAction<number>>;
-  setSat: Dispatch<SetStateAction<number>>;
-  setLum: Dispatch<SetStateAction<number>>;
-  hue: number;
-  sat: number;
-  lum: number;
+	setHue: Dispatch<SetStateAction<number>>;
+	setSat: Dispatch<SetStateAction<number>>;
+	setLum: Dispatch<SetStateAction<number>>;
+	hue: number;
+	sat: number;
+	lum: number;
 }
 
 export const Slider: React.FC<SliderProps> = ({
-  setHue,
-  setSat,
-  setLum,
-  hue,
-  sat,
-  lum,
+	setHue,
+	setSat,
+	setLum,
+	hue,
+	sat,
+	lum,
 }) => {
-  return (
-    <div className="flex flex-col gap-2">
-      <input
-        value={hue}
-        type="range"
-        min="0"
-        max="360"
-        step="10"
-        onChange={(e) => setHue(parseInt(e.target.value))}
-      />
-      <input
-        value={sat}
-        type="range"
-        min="0"
-        max="100"
-        step="10"
-        onChange={(e) => setSat(parseInt(e.target.value))}
-      />
-      <input
-        value={lum}
-        type="range"
-        min="0"
-        max="100"
-        step="10"
-        onChange={(e) => setLum(parseInt(e.target.value))}
-      />
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-2">
+			<div className="flex flex-col gap-1">
+				<h1>Hue</h1>
+				<input
+					value={hue}
+					type="range"
+					min="0"
+					max="360"
+					onChange={(e) => setHue(parseInt(e.target.value))}
+				/>
+			</div>
+			<div className="flex flex-col gap-1">
+				<h1>Saturation</h1>
+
+				<input
+					value={sat}
+					type="range"
+					min="0"
+					max="100"
+					onChange={(e) => setSat(parseInt(e.target.value))}
+				/>
+			</div>
+			<div className="flex flex-col gap-1">
+				<h1>Lumination</h1>
+
+				<input
+					value={lum}
+					type="range"
+					min="0"
+					max="100"
+					onChange={(e) => setLum(parseInt(e.target.value))}
+				/>
+			</div>
+		</div>
+	);
 };
